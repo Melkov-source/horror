3ae84d195ef689d7110e4f243a7af3d44e7bd036
Dmitry Melkov <melkov@itibsoft.ru>
2025-05-02 17:19:11 +0300

    - feat: import newtonsoft.json and dootwen pro

--- unity/Assets/Code/Game/GameManager.cs
+++ unity/Assets/Code/Game/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using Code.Character;
 using Code.DI;
 using JetBrains.Annotations;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
