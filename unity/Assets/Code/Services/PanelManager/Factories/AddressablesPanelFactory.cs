07a3985cac4bdefae9c6754318cab099099b1955
Dmitry Melkov <melkov@itibsoft.ru>
2025-05-03 09:22:48 +0300

    - feat: addressables loading content for panel manager

---
+++ unity/Assets/Code/Services/PanelManager/Factories/AddressablesPanelFactory.cs
@@ -0,0 +1,21 @@
+﻿using Code.PanelManager.Attributes;
+using Code.PanelManager.Interfaces;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Code.PanelManager
+{
+	public class AddressablesPanelFactory : IPanelFactory
+	{
+		public IPanel Create(PanelAttribute meta)
+		{
+			var prefab = Addressables
+				.LoadAssetAsync<GameObject>(meta.AssetId)
+				.WaitForCompletion();
+			
+			var panel = Object.Instantiate(prefab);
+
+			return panel.GetComponent<PanelBase>();
+		}
+	}
+}
