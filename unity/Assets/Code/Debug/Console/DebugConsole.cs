40bbf311aba678192a0737c613d098cc665c83cc
Dmitry Melkov <melkov@itibsoft.ru>
2025-05-04 14:31:54 +0300

    - feat: create comment for docs planned logic

--- unity/Assets/Code/Debug/Console/DebugConsole.cs
+++ unity/Assets/Code/Debug/Console/DebugConsole.cs
@@ -1,5 +1,23 @@
 ﻿namespace Code.Debug.Console
 {
+	/*
+	 * call -c DebugConsolePanel -m TestMethod -a "hello world"
+	 * call -c Debug ConsolePanel -m TestMethod -a "test" -i 1
+	 *
+	 * set -c DebugConsolePanel -m id -v "new value"
+	 * get -c DebugConsolePanel -m id
+	 *
+	 * -c -> Class contract
+	 * -m -> Class member
+	 * -a -> Arguments/Parameters for method
+	 * -i -> identity instance
+	 * -v -> field/property (<T>back_field) value
+	 *
+	 * call -> invoke method
+	 * set/get -> set/get value in members: field/property
+	 * list -m -> list members in contract class
+	 * list -c -> list instances contracts
+	 */
 	public class DebugConsole
 	{
 		public static void Initialize()
@@ -11,7 +29,6 @@
 
 		private void Start()
 		{
-			
 		}
 	}
 }
